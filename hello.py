7b52afaf41f0c9ac7e91349485dfc6f08b4cad4d
drewpaterson <drew.j.paterson@gmail.com>
2018-05-23 13:58:02 +1000

    random hello.py updates

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
 print("Hello World")
+
+a = 2
+b = 3
+
+
+sum = a + b
+print(a + b)
+print(sum)
+
+mylist = [a, b]
+
+print(mylist)
+
+print(mylist[0])
+
+def drewsfunction(dog):
+    print("dogs are unreal, I want " + str(dog) + " dogs")
+
+drewsfunction(5)
